3d410eb2ff42456108f551a59e53970abf4d9f15
Valetyn <valentindikhtyarenko2004@gmail.com>
2025-09-14 16:07:32 +0900

    Fixed tablet overlay

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,6 +3,6 @@ import './Hero.scss'
 export default () => {
 
   return (
-    <section></section>
+    <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, eius? Ex cum quasi saepe odio omnis quibusdam dolor cupiditate voluptatum non animi nostrum unde adipisci asperiores, itaque quia corporis eveniet?</section>
   )
 }
