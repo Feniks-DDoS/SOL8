288492d5fb9725593138c86836fdb23ce1370a82
Valetyn <valentindikhtyarenko2004@gmail.com>
2025-09-16 23:52:49 +0900

    Fixed overlay menu 2

--- src/modules/OverlayMenu.js
+++ src/modules/OverlayMenu.js
@@ -3,7 +3,6 @@ class OverlayMenu {
     root: '[data-js-header]',
     overlay: '[data-js-header-overlay]',
     burgerButton: '[data-js-header-burger-button]',
-    innerBg: `[data-js-header-inner]`,
   }
 
   stateClasses = {
@@ -17,7 +16,6 @@ class OverlayMenu {
       return
     }
     this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
-    this.innerBgElement = this.rootElement.querySelector(this.selectors.innerBg)
     this.burgerButtonElement = this.rootElement.querySelector(
       this.selectors.burgerButton
     )
@@ -27,7 +25,6 @@ class OverlayMenu {
   onBurgerButtonClick = () => {
     this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
     this.overlayElement.classList.toggle(this.stateClasses.isActive)
-    this.innerBgElement.classList.toggle(this.stateClasses.isActive)
     document.documentElement.classList.toggle(this.stateClasses.isLock)
   }
 
