48de18cf511da8cc6e6b4a2b9185ff1656b0b527
Valetyn <valentindikhtyarenko2004@gmail.com>
2025-09-19 18:02:38 +0900

    manifest fix

--- src/global.jsx
+++ src/global.jsx
@@ -3,10 +3,10 @@ import { Head } from 'minista'
 import Header from '@/layouts/Header'
 import Content from '@/layouts/Content'
 import Footer from '@/layouts/Footer'
-import manifest from '@/assets/favicons/site.webmanifest'
-import favicon from '/public/images/favicon.svg'
-import icon192 from '@/assets/favicons/android-chrome-192x192.png'
-import icon512 from '@/assets/favicons/android-chrome-512x512.png'
+import manifest from '/public/favicons/site.webmanifest'
+import favicon from '/public/favicons/favicon.svg'
+import icon192 from '/public/favicons/android-chrome-192x192.png'
+import icon512 from '/public/favicons/android-chrome-512x512.png'
 import AsideActions from './components/AsideActions'
 import SliderImage from "@/assets/images/hero/avif/1hero.avif"
 import FormMessage from './components/FormMeassge'
@@ -21,6 +21,8 @@ export default (props) => {
         <script src="/src/main.js" type="module" />
         <link rel="icon" sizes='16x16' href={favicon} />
         <link rel="icon" sizes='32x32' href={favicon} />
+        <link rel="icon" sizes='192x192' href={icon192} />
+        <link rel="icon" sizes='512x512' href={icon512} />
         <link rel="preload" as="image" href={SliderImage} />
         <link rel="manifest" href={manifest} />
       </Head>
