4a5d89efdf4cefd8dbadb70a4586f458fefc9c05
Valetyn <valentindikhtyarenko2004@gmail.com>
2025-11-05 14:27:02 +0900

    Fixed preloader src

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,11 +1,12 @@
 import "./Preloader.scss"
+import PreloaderImage from "./public/images/logoAnimation.gif"
 
 export default () => {
   return (
     <>
      <div className="preloader" data-js-preloader=""> 
         <div className="preloader__inner">
-            <img src="./public/images/logoAnimation.gif" alt="Loader..." width={500} height={500} className="preloader__image" />
+            <img src={PreloaderImage} alt="Loader..." width={500} height={500} className="preloader__image" />
         </div>
      </div>
     </>
